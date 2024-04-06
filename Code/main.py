ab6088a5de7067893a0c4ef3e56b3f4fdd7c11c1
Omar Ben Mariem <omarbenmariem12@outlook.fr>
2024-04-06 00:16:10 +0000

    projet Semestriel StarDew Valley clone pygame

---
+++ Code/main.py
@@ -0,0 +1,23 @@
+import pygame 
+from level import Level
+from settings import *
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        pygame.display.set_caption("OmarDew Valley")
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+    
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    
+            dt = self.clock.tick() / 1000
+            self.level.run(dt)
+            pygame.display.update() 
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
